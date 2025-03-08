d7483f89a90a6e78d92cc7852bc10770ad296509
joetaro736 <joequegyra@gmail.com>
2025-03-08 14:34:23 -0300

    add

---
+++ trabalhodemaker.py
@@ -0,0 +1,5 @@
+import streamlit as st
+st.title('robôs na sociedade')
+st.write("joe paulino, maria helena, guilherme simões, gustavo de melo margoni e rebeca rueda")
+st.image('images.jpeg')
+st.title('os robôs podem tanto ajudar como atrapalhar')
