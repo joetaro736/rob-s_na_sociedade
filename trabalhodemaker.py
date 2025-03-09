934c38f879b0fcca5712c8cbac2791906527bc96
joetaro736 <joequegyra@gmail.com>
2025-03-08 22:36:53 -0300

    Add files via upload

--- trabalhodemaker.py
+++ trabalhodemaker.py
@@ -19,7 +19,7 @@ with tab1:
     st.markdown('## :green[6. Segurança:] Robôs de patrulha e monitoramento.')
 
 with tab2:   
-    tab3, tab4 = st.tabs(['problemas', 'soluções'])
+    tab3, tab4, tab5 = st.tabs(['problemas', 'soluções', 'principais afetados'])
     with tab3:
         st.title('problema dos robôs no dia a dia:')
         st.markdown('## :red[1. Desemprego:] A automação pode substituir empregos humanos, principalmente indústrias e serviços manuais.')
@@ -28,13 +28,19 @@ with tab2:
         st.markdown('## :red[4. Falhas técnicas e segurança:] Os robôs podem ser vulneraveis a falhas ou ataques cibernéticos, comprometendo a segurança e a eficiência.')
         st.markdown('## :red[5. Falta de Empatia e Humanização:] Robôs podem ser eficazes mas não possuem empatia ou emoção, oque pode prejudica áreas como atendimento ao cliente e cuidados com idosos.')
     with tab4:
-        st.title('as soluções pra esses pronlemas:')
+        st.title('as soluções pra esses problemas:')
         st.markdown('## :red[1. Desemprego:] A solução desse problema seria envestir em educação e qualificação para que os trabalhadores possam atuar em novas áreas, como programação, inteligência artificial e manutenção de robôs.')
         st.markdown('## :red[2. Alto custo inicial:] A solução desse problema poderia ser incentivos governamentais e avanços tecnológicos que podem tornar os robôs mais acessíveis ao longo do tempo. Além disso, modelos de aluguel ou compartilhamento podem ser uma alternativa.')
         st.markdown('## :red[3. Dependência excessiva da tecnologia:] A solução pra esse problema seria manter um equilibrio entre o uso da tecnologia e da capacidade humana de realizar tarefas de forma manual quando necessário.')
         st.markdown('## :red[4. Falhas técnicas e segurança:] A solução pra esse problema seria investir em segurança cibernética , atualizações constantes e manutenção preventiva para garantir bom funcionamento.')
         st.markdown('## :red[5. Falta de Empatia e Humanização:] A solução pra esse problema seria melhorar a inteligência artificial para que os robôs passem a agir de maneira mais natural e complementar a atuação humana.')
-
+    with tab5:
+        st.title('os maiores públicos afetados são:')
+        st.markdown('## :red[1. Trabalhadores:] A automação pode substituir empregos, afetando principalmente funções repetitivas.')
+        st.markdown('## :red[2. Empresas:] O auto custo do robô dificulta a adoção por pequenas e médias empresas.')
+        st.markdown('## :red[3. Consumidores:] Preocupação com a privacidade e segurança ao usar dispositivos robóticos.')
+        st.markdown('## :red[4. Governo:] Precisa criar regulamentações para o uso seguro e ético dos robôs.')
+        st.markdown('## :red[5. Indústria de tecnologia:] enfrenta desafios com falhas tecnológicas e inovações constantes.')
 
 st.sidebar.image('lasalle.png')
 st.sidebar.title('Robôs na sociedade')
